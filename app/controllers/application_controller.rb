69e6b45667fd18499e8085505ccf9a7fde0b728e
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-10-05 08:48:46 +0000

    [add] スタイルシートの適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
+  
+    def after_sign_out_path_for(resource)
+      about_path
+    end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'homes/index'
-  get 'homes/about'
+  get 'about', to: "homes#about", as: "about"
   devise_for :users
   resources :recipes, only:[:edit, :index, :show]
   resources :users, only:[:edit, :show, :update]
