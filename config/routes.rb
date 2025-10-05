ea70f4835e62bebfa29ad1bfc9f6adee01a3e4f1
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-10-05 07:03:19 +0000

    [add] cssにツールの導入

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'homes/about'
   devise_for :users
   resources :recipes, only:[:edit, :index, :show]
-  resources :users, only:[:edit, :show]
+  resources :users, only:[:edit, :show, :update]
   root to: "homes#top"
 end
