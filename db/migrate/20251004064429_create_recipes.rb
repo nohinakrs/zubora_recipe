7f7fcae4deba7a275b644f179bbafc0d023cf43b
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-10-05 07:43:29 +0000

    [add] ユーザー情報編集、レシピ投稿機能実装中

--- db/migrate/20251004064429_create_recipes.rb
+++ db/migrate/20251004064429_create_recipes.rb
@@ -1,7 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
-
+      t.string  :recipe_name
+      t.text    :recipe
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/migrate/20251004070740_add_devise_to_users.rb
+++ db/migrate/20251004070740_add_devise_to_users.rb
@@ -3,6 +3,8 @@
 class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
     change_table :users do |t|
+      t.string :name
+      t.text :introduction
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
