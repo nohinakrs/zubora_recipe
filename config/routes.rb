96dbf6031935f827aef0d743fd0c62866fa1ef0a
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-10-11 08:34:52 +0000

    [add] CRUD機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  resources :recipes, only:[:edit, :create, :new, :index, :show, :update,:destroy]
+  resources :recipes, only:[:edit, :create, :new, :index, :show, :update, :destroy]
   resources :users, only:[:edit, :show, :update]
   
   get "home/about" => "homes#about", as: "about"
