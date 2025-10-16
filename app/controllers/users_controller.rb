185b580c7f0e9d137f701ba46ca5b55d7acb9c75
EC2 Default User <ec2-user@ip-172-31-37-111.ap-northeast-1.compute.internal>
2025-10-16 11:45:52 +0000

    [add] レイアウト調整中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def edit
-    @user = User.find(params[:id])
+  @user = User.find(params[:id])
   end
 
   def show
@@ -9,8 +12,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    
       if @user.update(user_params)
        flash[:notice] = "You have updated user successfully."
        redirect_to user_path(@user.id)
@@ -28,7 +29,16 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  private
+  
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
